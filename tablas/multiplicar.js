d2e110dd96fc513302293e574b8a2e3918ee8c7c
alci19 <bytonalci@gmail.com>
2026-04-30 13:01:37 -0500

    Ahora en vez de probando; con el bucle for, cambiamos todo el html para mostrar la multiplicacion que queramos

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,5 +1,16 @@
 function generarTablas() {
     let contenedor = document.getElementById("contenedor"); 
-    contenedor.innerHTML = "<h1> PROBANDO </h1>";    
+    let contenido = "";
+    let numero = 5; //condicion del taller, se puede cambiar por cualquier numero 
+
+    contenido += `<h1>🌟 Tabla del ${numero} 🌟</h1>`;
+    contenido += `<div class="tabla">`;
+
+    for (let i = 1; i <= 10; i++) {
+        contenido += `<div class="fila">${numero} × ${i} = ${numero * i}</div>`;
+    }
+    contenido += `</div>`;
+    
+    contenedor.innerHTML = contenido;    
 
 }
